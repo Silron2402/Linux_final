da7614a6368c5845f643820559ef9945712cdcd6
Andrew Vataev <avataev@yandex.ru>
2026-01-17 23:18:29 +0300

    исправлен файл установки лидара  lidar_install.sh

--- scripts/lidar_install.sh
+++ scripts/lidar_install.sh
@@ -21,10 +21,11 @@ is_package_installed() {
 #Получение имени пользователя и адреса домашнего каталога
 USERNAME=$(whoami)
 USER_HOME=$(getent passwd "$USERNAME" | cut -d: -f6)
+USER_2=$(getent passwd "$SUDO_USER" | cut -d: -f6)
 
 # Путь к установочной директории
 WORKSPACE_DIR="$USER_HOME/ros2_ws"
-WORK_DIR="$USER_HOME/lidar_slamtec"
+WORK_DIR="$USER_2/lidar_slamtec"
 echo "Текущий пользователь: $(whoami)"
 echo "Домашний каталог: $HOME"
 echo "Рабочий каталог: $PWD"
